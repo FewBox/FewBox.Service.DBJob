ee74e27319561042d7eaae13f2584a3b0fe7b1a5
landpy <landpy@outlook.com>
2022-10-10 20:44:31 +0800

    Refine code.

--- build-nas.sh
+++ build-nas.sh
@@ -1,11 +1,14 @@
 echo 'Build DB Job...'
-DOCKER_REPO_SLUG=192.168.1.38:5000/fewbox/dbjob PROJECTNAME=FewBox.Service.DBJob
+DOCKER_REPO_SLUG=fewbox/dbjob PROJECTNAME=FewBox.Service.DBJob
+DOCKER_REPO_VERSION=v1
+DOCKER_REPO_IP=192.168.1.38
+DOCKER_REPO_PORT=5000
 dotnet restore $PROJECTNAME
 dotnet publish -c Release $PROJECTNAME/$PROJECTNAME.csproj
-cp ./Nas/Dockerfile $PROJECTNAME/bin/Release/net6.0/publish/Dockerfile
-cp ./Nas/.dockerignore $PROJECTNAME/bin/Release/net6.0/publish/.dockerignore
-cd $PROJECTNAME/bin/Release/net6.0/publish
-docker build -t $DOCKER_REPO_SLUG:v1 .
+cp Nas/* ./$PROJECTNAME/bin/Release/netcoreapp6.0/publish
+cp .dockerignore ./$PROJECTNAME/bin/Release/netcoreapp6.0/publish/.dockerignore
+cd $PROJECTNAME/bin/Release/netcoreapp6.0/publish
+docker build --no-cache -t $DOCKER_REPO_IP:$DOCKER_REPO_PORT/$DOCKER_REPO_SLUG:$DOCKER_REPO_VERSION .
+docker push $DOCKER_REPO_IP:$DOCKER_REPO_PORT/$DOCKER_REPO_SLUG:$DOCKER_REPO_VERSION
 cd ../../../../../
-docker push $DOCKER_REPO_SLUG:v1
 echo 'Finished!'
--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 echo 'Build DB Job...'
 DOCKER_REPO_SLUG=fewbox/dbjob PROJECTNAME=FewBox.Service.DBJob
+DOCKER_REPO_VERSION=v1
+DOCKER_REPO_IP=192.168.1.38
+DOCKER_REPO_PORT=5000
 dotnet restore $PROJECTNAME
-dotnet publish -c Release $PROJECTNAME/$PROJECTNAME.csproj -p:FileVersion=$TRAVIS_JOB_NUMBER
-cp ./Dockerfile $PROJECTNAME/bin/Release/net6.0/publish/Dockerfile
-cp ./.dockerignore $PROJECTNAME/bin/Release/net6.0/publish/.dockerignore
-cd $PROJECTNAME/bin/Release/net6.0/publish
-docker build -t $DOCKER_REPO_SLUG:latest .
-docker tag $DOCKER_REPO_SLUG:latest $DOCKER_REPO_SLUG:v1
+dotnet publish -c Release $PROJECTNAME/$PROJECTNAME.csproj
+cp Nas/* ./$PROJECTNAME/bin/Release/netcoreapp6.0/publish
+cp .dockerignore ./$PROJECTNAME/bin/Release/netcoreapp6.0/publish/.dockerignore
+cd $PROJECTNAME/bin/Release/netcoreapp6.0/publish
+docker build --no-cache -t $DOCKER_REPO_IP:$DOCKER_REPO_PORT/$DOCKER_REPO_SLUG:$DOCKER_REPO_VERSION .
+docker tag $DOCKER_REPO_IP:$DOCKER_REPO_PORT/$DOCKER_REPO_SLUG:$DOCKER_REPO_VERSION $DOCKER_REPO_SLUG:latest
+docker push $DOCKER_REPO_IP:$DOCKER_REPO_PORT/$DOCKER_REPO_SLUG:$DOCKER_REPO_VERSION
+docker push $DOCKER_REPO_IP:$DOCKER_REPO_PORT/$DOCKER_REPO_SLUG:latest
 #docker push $PT_DOCKER_REPO_SLUG:v1
 cd ../../../../../
 echo 'Finished!'
